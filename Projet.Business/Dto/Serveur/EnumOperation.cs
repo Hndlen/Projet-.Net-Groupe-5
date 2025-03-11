9d85c4df37472783d70c5e1bb9991cb43e5ed3be
thierry B <bossouthierry@yahoo.fr>
2025-03-11 15:47:17 +0100

    ajout de l'insertion dans la bdd à partir d'un fichier json

---
+++ Projet.Business/Dto/Serveur/EnumOperation.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projet.Business.Dto.Serveur
+{
+    public enum EnumOperation
+    {
+        Retrait,
+        Depot,
+        Facture
+    }
+}
