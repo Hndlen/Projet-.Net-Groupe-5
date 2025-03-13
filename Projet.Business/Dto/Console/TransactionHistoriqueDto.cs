aa9ed24140712fb5bfe1ed1d2e017f7f0fc35f26
thierry B <bossouthierry@yahoo.fr>
2025-03-13 15:04:40 +0100

    correction route push trasactionhistorique, changement nom et params route

--- Projet.Business/Dto/Console/TransactionHistoriqueDto.cs
+++ Projet.Business/Dto/Console/TransactionHistoriqueDto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,10 +10,15 @@ namespace Projet.Business.Dto.Console
     public class TransactionHistoriqueDto
     {
         public int Id { get; set; }
+        [Required]
         public string CompteCarteId { get; set; }
+        [Required]
         public string NumeroCarteBancaire { get; set; }
+        [Required]
         public double MontantOperation { get; set; }
+        [Required]
         public string TypeOperation { get; set; }
+        [Required]
         public DateTime DateOperation { get; set; }
         public string Devise { get; set; }
     }
